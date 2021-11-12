2ad72c88615e9d575589f5a4d5bed679d8fe98be
JuanitoDev <devotojuanmi@gmail.com>
2021-11-12 16:36:01 +0100

    delete button done

--- routers/spaces.js
+++ routers/spaces.js
@@ -29,5 +29,23 @@ router.get("/:id", async (request, response, next) => {
   }
 });
 
+router.delete("/story/:story_id", async (request, response, next) => {
+  try {
+    // delete story with id == story_id if this user is the owner of the space of the story
+    const params = request.params;
+    const id = params.story_id;
+    // console.log("what's inside Id?", id);
+    // console.log("what's inside the params?", params);
+
+    const target_story = await Story.findByPk(id);
+    console.log("found", target_story);
+    await target_story.destroy();
+    // console.log("Got the spaces", spaces);
+    response.send(id);
+  } catch (e) {
+    next(e);
+  }
+});
+
 //EXPORT
 module.exports = router;
